dc9f8c70e6368f97ec2223e0a5caec7e73b68b78
George W F Santos <gweverton21@hotmail.com>
2020-05-29 18:46:52 -0300

    start implementing upload feature

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import checkAuthentication from '../middlewares/checkAuthentication';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +29,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  checkAuthentication,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
