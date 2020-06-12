7ecfdcd844956e073d954b681442d59e53697ebe
George W F Santos <gweverton21@hotmail.com>
2020-06-11 23:10:16 -0300

    start frontend project and set up global style

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>GoBarber</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
