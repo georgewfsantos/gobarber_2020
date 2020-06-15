5c9b1c6b14a8a2f8685ab24158454d3668ea87be
George W F Santos <gweverton21@hotmail.com>
2020-06-15 18:52:35 -0300

    create useAuth function in auth context

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,10 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
@@ -7,26 +12,52 @@ interface SignInCredentials {
 }
 
 interface AuthContextInfo {
-  name: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextInfo>({} as AuthContextInfo);
 
 const AuthProvider: React.FC = ({ children }) => {
+  const [authData, setAuthData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setAuthData({ token, user });
   }, []);
   return (
-    <AuthContext.Provider value={{ name: 'Diego', signIn }}>
+    <AuthContext.Provider value={{ user: authData.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextInfo {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be wrapped in AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
