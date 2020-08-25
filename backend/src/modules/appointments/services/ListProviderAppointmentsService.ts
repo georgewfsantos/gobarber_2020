bdf70047a59d918dfd04ecb764c0f349bda865e8
George W F dos Santos <gweverton21@hotmail.com>
2020-08-25 14:54:24 -0300

    implement cache for appointment creation functionality

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -26,19 +26,24 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
-    const cachedData = await this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}${year}-${month}-${day}`;
 
-    console.log(cachedData);
-    const appointments = await this.appointmentsRepository.findAllOnDayByProvider(
-      {
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
+    );
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllOnDayByProvider({
         provider_id,
         day,
         month,
         year,
-      },
-    );
+      });
 
-    // await this.cacheProvider.save('asd', 'asd');
+      console.log('Buscou do banco');
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
