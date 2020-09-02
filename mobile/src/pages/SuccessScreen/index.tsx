13b9e0d06a02d1e2c7d3d95f983d3fdc7d983dd2
George W F dos Santos <gweverton21@hotmail.com>
2020-09-02 14:49:18 -0300

    build and style Success screen for appointment creation

--- mobile/src/pages/SuccessScreen/index.tsx
+++ mobile/src/pages/SuccessScreen/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useCallback } from 'react';
+import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const SuccessScreen: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkButtonPress = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <Container>
+        <Icon name="check" size={80} color="#04d361" />
+
+        <Title>Agendamento concluído</Title>
+        <Description>Terça, dia 14 de março de 2020 às 12:00 h</Description>
+
+        <OkButton onPress={handleOkButtonPress}>
+          <OkButtonText>Ok</OkButtonText>
+        </OkButton>
+      </Container>
+    </>
+  );
 };
 
 export default SuccessScreen;
